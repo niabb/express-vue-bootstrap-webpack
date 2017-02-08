495d05da5c3f9328dd85e98ec87ca180ae065e2a
Nicolas <nicolas.abbal@gmail.com>
2017-02-08 16:57:32 +0100

    Initial commit.

---
+++ public/index.js
@@ -0,0 +1,6 @@
+import _ from 'vue';
+import _ from 'vue-resource';
+import _ from 'jquery';
+import _ from 'bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+  entry: './public/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      { test: /\.(woff|woff2)$/, loader:"url-loader?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
+    ]
+  },
+  plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ]
+
+};
